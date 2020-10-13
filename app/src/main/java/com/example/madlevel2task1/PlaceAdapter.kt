1f3a40f0d61be6f4db36f68489f7905a56e32644
Bob Anthony Imuekemhe <bob.imuekemhe@hva.nl>
2020-10-13 05:17:27 +0200

    updated PlaceAdapter.kt

--- app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
+++ app/src/main/java/com/example/madlevel2task1/PlaceAdapter.kt
@@ -1,4 +1,34 @@
 package com.example.madlevel2task1
 
-class PlaceAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.databinding.ItemPlaceBinding
+
+class PlaceAdapter(private val reminders: List<Place>) : RecyclerView.Adapter<PlaceAdapter.ViewHolder>(){
+
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            val binding = ItemPlaceBinding.bind(itemView)
+
+            fun databind(place: Place) {
+                binding.tvPlace.text = place.name
+                binding.ivPlace.setImageResource(place.imageResId)
+            }
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
